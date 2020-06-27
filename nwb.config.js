acece52332f84156c5d277205110ab7d67f2a3c8
cjquines <cj@cjquines.com>
2020-06-27 11:34:05 -0400

    add typescript

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  type: 'web-app'
-}
+  type: "web-app",
+  webpack: {
+    config(config) {
+      config.resolve.extensions = [".ts", ".tsx", ".js", ".jsx"];
+      config.module.rules.push({
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [{ loader: "awesome-typescript-loader" }],
+      });
+      return config;
+    },
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { fabric } from "fabric";
+
+let app = document.querySelector("#app");
+
+app.innerHTML = "<h2>Welcome to qboard</h2>";
