237eb82908806412c3dca7d800eac5af09c203a5
cjquines <cj@cjquines.com>
2020-07-01 00:41:37 -0400

    style pagination overlay

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -4,11 +4,55 @@ import QBoard, { QBoardState } from "./qboard";
 import { Tool } from "./tools";
 import { Dash, Stroke, Fill } from "./styles";
 
-interface OverlayProps {
-  qboard: QBoard;
-}
+const Pagination = (props: {
+  previousPage: () => Promise<void>;
+  nextOrNewPage: () => Promise<void>;
+  loadPage: (number) => Promise<void>;
+  currentPage: number;
+  totalPages: number;
+}) => {
+  const [value, setValue] = useState(0);
+  const [width, setWidth] = useState("0.6em");
 
-const Overlay = (props: OverlayProps) => {
+  useEffect(() => {
+    setValue(props.currentPage);
+    setWidth(0.6 * props.currentPage.toString().length + "em");
+  }, [props]);
+
+  const onSubmit = (e) => {
+    e && e.preventDefault();
+    const page = Number(value);
+    if (!page || page > props.totalPages) {
+      setValue(props.currentPage);
+    } else {
+      props.loadPage(page - 1);
+    }
+  };
+
+  const onChange = (e) => setValue(e.target.value);
+
+  return (
+    <div className="pagination">
+      <button onClick={props.previousPage}>
+        <i className="fas fa-caret-left" />
+      </button>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          value={value}
+          style={{ width }}
+        />
+      </form>
+      <span className="total-pages"> / {props.totalPages}</span>
+      <button onClick={props.nextOrNewPage}>
+        <i className="fas fa-caret-right" />
+      </button>
+    </div>
+  );
+};
+
+const Overlay = (props: { qboard: QBoard }) => {
   const qboard = props.qboard;
   const [state, setState] = useState<QBoardState>({
     currentPage: 0,
@@ -22,28 +66,13 @@ const Overlay = (props: OverlayProps) => {
 
   return (
     <div className="overlay">
-      <div className="pagination">
-        <span>
-          {state.currentPage} / {state.totalPages}
-        </span>
-        <button onClick={() => {
-          qboard.pages.export()
-        }}>print?</button>
-      </div>
-      <div className="toolbar">
-        <button onClick={() => qboard.switchTool(Tool.Move)}>move</button>
-        <button onClick={() => qboard.switchTool(Tool.Line)}>line</button>
-        <button onClick={() => qboard.switchTool(Tool.Rectangle)}>
-          rectangle
-        </button>
-        <button onClick={() => qboard.switchTool(Tool.Ellipse)}>ellipse</button>
-        <button onClick={() => qboard.switchTool(Tool.Pen)}>pen</button>
-        <button onClick={() => qboard.switchTool(Tool.Eraser)}>eraser</button>
-        <button onClick={() => qboard.history.undo()}>undo</button>
-        <button onClick={() => qboard.history.redo()}>redo</button>
-        <button onClick={() => qboard.clipboard.copy()}>copy</button>
-        <button onClick={() => qboard.clipboard.paste()}>paste</button>
-      </div>
+      <Pagination
+        previousPage={qboard.pages.previousPage}
+        nextOrNewPage={qboard.pages.nextOrNewPage}
+        loadPage={qboard.pages.loadPage}
+        currentPage={state.currentPage}
+        totalPages={state.totalPages}
+      />
     </div>
   );
 };
