f8c806dcd359b148fb290b669a5dfad55115142a
cjquines <cj@cjquines.com>
2020-07-20 14:53:59 -0400

    change default keybindings
    be gone, chording

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -7,41 +7,48 @@ import { ClipboardHandler } from "./clipboard";
 import { HistoryHandler } from "./history";
 import { Dash, Stroke, Fill } from "./styles";
 
-const toolMap = {
-  e: Tool.Ellipse,
-  r: Tool.Rectangle,
-  d: Tool.Pen,
-  x: Tool.Eraser,
-  c: Tool.Line,
-  v: Tool.Move,
-};
+const defaultKeys = {
+  "w": "copy",
+  "e": "blue",
+  "r": "green",
+  "a": "previousPage",
+  "s": "nextPage",
+  "d": "pen",
+  "f": "undo",
+  "g": "paste",
+  "z": "resetStyles",
+  "x": "eraser",
+  "c": "line",
+  "v": "move",
 
-const dashMap = {
-  w: Dash.Dotted,
-  a: Dash.Dashed,
-  s: Dash.Solid,
-};
+  "shift + q": "dotted",
+  "shift + w": "transparent",
+  "shift + e": "ellipse",
+  "shift + r": "rectangle",
+  "shift + a": "dashed",
+  "shift + s": "halfFilled",
+  "shift + d": "black",
+  "shift + f": "redo",
+  "shift + z": "solid",
+  "shift + x": "filled",
+  "shift + c": "yellow",
+  "shift + v": "orange",
 
-const strokeMap = {
-  e: Stroke.Blue,
-  r: Stroke.Green,
-  f: Stroke.Black,
-  c: Stroke.Yellow,
-  v: Stroke.Orange,
-};
-
-const fillMap = {
-  d: Fill.Solid,
-  f: Fill.Solid,
-  g: Fill.HalfSolid,
-  space: null,
+  "ctrl + y": "redo",
+  "ctrl + a": "selectAll",
+  "ctrl + s": "save",
+  "ctrl + d": "duplicate",
+  "ctrl + z": "undo",
+  "ctrl + x": "cut",
+  "ctrl + c": "copy",
+  "ctrl + v": "paste",
 };
 
 export class KeyboardHandler {
   canvas: fabric.Canvas;
   combo: string[] = [];
   actionMap: any;
-  ctrlMap: any;
+  keyMap: any;
 
   constructor(
     public switchTool: (tool: Tool) => Promise<void>,
@@ -58,34 +65,21 @@ export class KeyboardHandler {
     this.canvas = this.pages.canvas;
 
     this.actionMap = {
-      w: clipboard.copy,
-      a: pages.previousPage,
-      s: pages.nextOrNewPage,
-      f: history.undo,
-      g: clipboard.paste,
-      z: () => {
-        this.setStyle(Dash.Solid, Stroke.Black, Fill.Transparent);
+      previousPage: pages.previousPage,
+      nextPage: pages.nextOrNewPage,
+      save: pages.export,
+
+      undo: history.undo,
+      redo: history.redo,
+
+      copy: clipboard.copy,
+      paste: clipboard.paste,
+
+      deselect: () => {
+        this.canvas.discardActiveObject();
+        this.canvas.requestRenderAll();
       },
-    };
-
-    const keys = "wertasdfgxcvz".split("");
-    for (const key of keys) {
-      keyboardJS.bind(
-        key,
-        (e) => this.combo.push(key),
-        (e) => this.read()
-      );
-
-      keyboardJS.bind(
-        `space + ${key}`,
-        (e) => this.combo.push(key, "space"),
-        (e) => this.read()
-      );
-    }
-
-    this.ctrlMap = {
-      y: history.redo,
-      a: () => {
+      selectAll: () => {
         this.canvas.setActiveObject(
           new fabric.ActiveSelection(this.canvas.getObjects(), {
             canvas: this.canvas,
@@ -93,29 +87,39 @@ export class KeyboardHandler {
         );
         this.canvas.requestRenderAll();
       },
-      s: pages.export,
-      d: () => {
+      duplicate: () => {
         this.clipboard.copy();
         this.clipboard.paste();
       },
-      z: history.undo,
-      x: clipboard.cut,
-      c: clipboard.copy,
-      v: clipboard.paste,
-    };
 
-    for (const key in this.ctrlMap) {
-      keyboardJS.bind(`ctrl + ${key}`, (e) => this.ctrlMap[key]());
+      pen: () => this.switchTool(Tool.Pen),
+      eraser: () => this.switchTool(Tool.Eraser),
+      ellipse: () => this.switchTool(Tool.Ellipse),
+      rectangle: () => this.switchTool(Tool.Rectangle),
+      move: () => this.switchTool(Tool.Move),
+
+      dotted: () => this.setDash(Dash.Dotted),
+      dashed: () => this.setDash(Dash.Dashed),
+      solid: () => this.setDash(Dash.Solid),
+
+      blue: () => this.setStroke(Stroke.Blue),
+      green: () => this.setStroke(Stroke.Green),
+      yellow: () => this.setStroke(Stroke.Yellow),
+      orange: () => this.setStroke(Stroke.Orange),
+      black: () => this.setStroke(Stroke.Black),
+
+      transparent: () => this.setFill(Fill.Transparent),
+      halfFilled: () => this.setFill(Fill.HalfSolid),
+      filled: () => this.setFill(Fill.Solid),
+
+      resetStyles: () => this.setStyle(Dash.Solid, Stroke.Black, Fill.Transparent),
     }
 
-    keyboardJS.bind("shift + f", (e) => {
-      this.history.redo();
-    });
+    this.keyMap = defaultKeys;
 
-    keyboardJS.bind("esc", (e) => {
-      this.canvas.discardActiveObject();
-      this.canvas.requestRenderAll();
-    });
+    for (const key in this.keyMap) {
+      keyboardJS.bind(`${key}`, (e) => this.actionMap[this.keyMap[key]]());
+    }
 
     keyboardJS.bind(
       "shift",
@@ -128,40 +132,15 @@ export class KeyboardHandler {
     );
   }
 
-  read = async (): Promise<void> => {
-    if (!this.combo.length) return;
-    const combo = Array.from(new Set(this.combo));
-    this.combo = [];
-    if (combo.length === 1) {
-      this.readSingle(combo[0]);
-    } else if (combo.length <= 3) {
-      this.readStyle(combo);
-    }
-  };
+  setDash = async (dash: Dash): Promise<void> => {
+    this.setStyle(dash, null, null);
+  }
 
-  readSingle = async (key: string): Promise<void> => {
-    if (key in toolMap) {
-      this.switchTool(toolMap[key]);
-    } else if (key in this.actionMap) {
-      this.actionMap[key]();
-    }
-  };
+  setStroke = async (stroke: Stroke): Promise<void> => {
+    this.setStyle(null, stroke, null);
+  }
 
-  readStyle = async (keys: string[]): Promise<void> => {
-    let dash = null,
-      stroke = null,
-      fill = Fill.Transparent,
-      badKey = false;
-    keys.forEach((key) => {
-      const dChange = dashMap.hasOwnProperty(key),
-        sChange = strokeMap.hasOwnProperty(key),
-        fChange = fillMap.hasOwnProperty(key);
-      if (!(dChange || sChange || fChange)) badKey = true;
-      if (dChange) dash = dashMap[key];
-      if (sChange) stroke = strokeMap[key];
-      if (fChange) fill = fillMap[key];
-    });
-    if (badKey) return;
-    this.setStyle(dash, stroke, fill);
-  };
+  setFill = async (fill: Fill): Promise<void> => {
+    this.setStyle(null, null, fill);
+  }
 }
