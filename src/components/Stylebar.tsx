95083f93a0abd3af85aeb4348d5f1ecb14cc2771
cjquines <cj@cjquines.com>
2020-10-08 23:32:55 -0400

    factor out style menu to StyleMenu

--- src/components/Stylebar.tsx
+++ src/components/Stylebar.tsx
@@ -1,85 +1,11 @@
 import React, { useEffect, useState } from "react";
 
-import { Dash, Stroke, Fill, Style } from "../lib/styles";
+import { Style } from "../lib/styles";
 import { Action } from "../lib/action";
 
 import { Visibility } from "./Overlay";
-import Icon from "./Icon";
+import StyleMenu from "./StyleMenu";
 import OverlayButton from "./OverlayButton";
-
-const DashStyle = (props: {
-  dashStyle: Dash;
-  callback: (Action) => Promise<void>;
-}) => {
-  const dashes = [Action.Solid, Action.Dashed, Action.Dotted];
-
-  return (
-    <div className="style">
-      <button className="inactive">{Icon[dashes[props.dashStyle]]}</button>
-      <div className="substyle">
-        {dashes.map((action) => (
-          <OverlayButton
-            action={action}
-            callback={props.callback}
-            key={action}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const StrokeStyle = (props: {
-  strokeStyle: string;
-  callback: (Action) => Promise<void>;
-}) => {
-  const strokes = [
-    Action.Black,
-    Action.Blue,
-    Action.Green,
-    Action.Yellow,
-    Action.Orange,
-  ];
-
-  return (
-    <div className="style">
-      <button className="inactive">
-        <i className="fas fa-circle" style={{ color: props.strokeStyle }} />
-      </button>
-      <div className="substyle">
-        {strokes.map((action) => (
-          <OverlayButton
-            action={action}
-            callback={props.callback}
-            key={action}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const FillStyle = (props: {
-  fillStyle: Fill;
-  callback: (Action) => Promise<void>;
-}) => {
-  const fills = [Action.Transparent, Action.Filled, Action.HalfFilled];
-
-  return (
-    <div className="style">
-      <button className="inactive">{Icon[fills[props.fillStyle]]}</button>
-      <div className="substyle">
-        {fills.map((action) => (
-          <OverlayButton
-            action={action}
-            callback={props.callback}
-            key={action}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
 
 const Stylebar = (props: {
   currentStyle: Style;
@@ -104,18 +30,7 @@ const Stylebar = (props: {
       {actions.map((action) => (
         <OverlayButton action={action} callback={props.doAction} key={action} />
       ))}
-      <DashStyle
-        dashStyle={props.currentStyle.dash}
-        callback={props.doAction}
-      />
-      <StrokeStyle
-        strokeStyle={props.currentStyle.stroke}
-        callback={props.doAction}
-      />
-      <FillStyle
-        fillStyle={props.currentStyle.fill}
-        callback={props.doAction}
-      />
+      <StyleMenu currentStyle={props.currentStyle} doAction={props.doAction} />
       {mobileMethods.map((action) => (
         <OverlayButton
           action={
