a254d0809fe86c7693fc9cea8839adab6dd0dfff
pihart <avi.mehra1@gmail.com>
2020-10-13 13:53:50 -0700

    Add optional chaining

--- src/lib/qboard.ts
+++ src/lib/qboard.ts
@@ -129,16 +129,15 @@ export default class QBoard {
   }
 
   updateState = (): void => {
-    this.callback &&
-      this.callback({
-        currentPage: this.pages.currentIndex + 1,
-        totalPages: this.pages.pagesJson.length,
-        currentTool: this.currentTool,
-        currentStyle: this.currentStyle,
-        canUndo: Boolean(this.history.history.length),
-        canRedo: Boolean(this.history.redoStack.length),
-        keyMap: this.keyboard.keyMap,
-      });
+    this?.callback?.({
+      currentPage: this.pages.currentIndex + 1,
+      totalPages: this.pages.pagesJson.length,
+      currentTool: this.currentTool,
+      currentStyle: this.currentStyle,
+      canUndo: Boolean(this.history.history.length),
+      canRedo: Boolean(this.history.redoStack.length),
+      keyMap: this.keyboard.keyMap,
+    });
   };
 
   switchTool = async (tool: Tool): Promise<void> => {
@@ -215,7 +214,9 @@ export default class QBoard {
     iEvent.e.preventDefault();
     this.updateCursor(iEvent);
     this.dragLeave(iEvent.e as DragEvent);
-    const imgs = await this.pages.processFiles((iEvent.e as DragEvent).dataTransfer.files);
+    const imgs = await this.pages.processFiles(
+      (iEvent.e as DragEvent).dataTransfer.files
+    );
     return this.history.add(imgs.flat());
   };
 
