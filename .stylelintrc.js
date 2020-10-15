b09b1895c059fbdc5c3af54e4fb2957c3701b4f2
pihart <avi.mehra1@gmail.com>
2020-10-15 12:39:06 -0700

    Add scss support for stylelint
    https://github.com/kristerkari/stylelint-scss#list-of-rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: "stylelint-config-standard",
+  plugins: ["stylelint-scss"],
+  rules: {
+    "at-rule-blacklist": ["debug"],
+    "at-rule-no-unknown": {
+      ignoreAtRules: ["function", "if", "each", "include", "mixin"],
+    },
+  },
 };
