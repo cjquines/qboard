a8011eae3192a09ab2f87a4f101f1651c412d079
pihart <avi.mehra1@gmail.com>
2020-12-14 15:44:47 -0800

    Await async calls in objectModified

--- src/lib/qboard.ts
+++ src/lib/qboard.ts
@@ -251,8 +251,8 @@ export default class QBoard {
     !this.history.locked && this.history.store(e.selected);
 
   objectModified = async (e: any): Promise<void> => {
-    this.history.modify(e.target._objects || [e.target]);
-    this.history.store(e.target._objects || [e.target]);
+    await this.history.modify(e.target._objects || [e.target]);
+    await this.history.store(e.target._objects || [e.target]);
   };
 
   updateCursor = (e: fabric.IEvent): void => {
