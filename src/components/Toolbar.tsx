d7f6a7301b356fb457db41cbfca4739c00c68488
pihart <avi.mehra1@gmail.com>
2021-01-31 23:22:20 -0800

    Revert Toolbar to function syntax

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -6,36 +6,34 @@ import { ToolHandler } from "../lib/tools";
 import { Visibility } from "./Overlay";
 import OverlayButton from "./OverlayButton";
 
-class Toolbar extends React.Component<{
+const Toolbar = (props: {
   currentTool: ToolHandler;
   handlers: { [key: string]: ToolHandler };
   doAction: (action: Action) => Promise<void>;
   visibility: Visibility;
-}> {
-  render(): JSX.Element {
-    const items = [
-      { tool: this.props.handlers.Move, action: Action.Move },
-      { tool: this.props.handlers.Pen, action: Action.Pen },
-      { tool: this.props.handlers.Eraser, action: Action.Eraser },
-      { tool: this.props.handlers.Laser, action: Action.Laser },
-      { tool: this.props.handlers.Line, action: Action.Line },
-      { tool: this.props.handlers.Rectangle, action: Action.Rectangle },
-      { tool: this.props.handlers.Ellipse, action: Action.Ellipse },
-    ];
+}): JSX.Element => {
+  const items = [
+    { tool: props.handlers.Move, action: Action.Move },
+    { tool: props.handlers.Pen, action: Action.Pen },
+    { tool: props.handlers.Eraser, action: Action.Eraser },
+    { tool: props.handlers.Laser, action: Action.Laser },
+    { tool: props.handlers.Line, action: Action.Line },
+    { tool: props.handlers.Rectangle, action: Action.Rectangle },
+    { tool: props.handlers.Ellipse, action: Action.Ellipse },
+  ];
 
-    return (
-      <div className={`toolbar visibility-${this.props.visibility}`}>
-        {items.map(({ tool, action }) => (
-          <OverlayButton
-            action={action}
-            callback={this.props.doAction}
-            className={tool.isActive() ? "active" : undefined}
-            key={action}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={`toolbar visibility-${props.visibility}`}>
+      {items.map(({ tool, action }) => (
+        <OverlayButton
+          action={action}
+          callback={props.doAction}
+          className={tool.isActive() ? "active" : undefined}
+          key={action}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Toolbar;
