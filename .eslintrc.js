496af5a46eb2682ce361ca4f43b3098395950abb
pihart <avi.mehra1@gmail.com>
2021-03-12 13:08:19 -0800

    Run eslint only on src/

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,12 +14,7 @@ module.exports = {
     "plugin:import/errors",
     "plugin:import/warnings",
   ],
-  ignorePatterns: [
-    "dist/**/*",
-    "node_modules/**/*",
-    "nwb.config.js",
-    ".eslintrc.js",
-  ],
+  ignorePatterns: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
