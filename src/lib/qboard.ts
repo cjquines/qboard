231f091ea594c05999a954792e99fff777b81b13
pihart <avi.mehra1@gmail.com>
2021-03-17 22:46:32 -0700

    Fix open action

--- src/lib/qboard.ts
+++ src/lib/qboard.ts
@@ -66,6 +66,16 @@ export default class QBoard {
   strict = false;
   callback: ((state: QBoardState) => void) | undefined;
 
+  /**
+   * Intentionally mutable global state object
+   */
+  public globalState: {
+    /**
+     * A ref to the global input element used for file input
+     */
+    fileInputRef?: React.RefObject<HTMLInputElement>;
+  } = {};
+
   constructor(
     public canvasElement: HTMLCanvasElement & HTMLChildElement,
     public baseCanvasElement: HTMLCanvasElement & HTMLChildElement,
@@ -121,7 +131,8 @@ export default class QBoard {
       this.files,
       this.history,
       this.clipboard,
-      this.style.set
+      this.style.set,
+      this.globalState
     );
     this.keyboard = new KeyboardHandler(
       this.action.doAction,
