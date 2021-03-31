568d83c6c4fdd3ff17495c091acc09e544f7933b
pihart <avi.mehra1@gmail.com>
2021-03-31 16:47:56 -0700

    Expose the QBoard instance to the public by attaching it to window as window.qboard
    Provides a basic developer interface.
    Resolves https://github.com/cjquines/qboard/issues/89

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import QBoard from "./lib/qboard";
 
 import "./main.scss";
 
-const qboard = new QBoard(
+// The live [[`QBoard`]] instance, exposed to the public as a basic developer interface.
+// Technically, the developer-user gets less control over private fields,
+// but from a power user standpoint this is likely not an issue;
+// no user should be writing scripts that depend on private fields.
+window.qboard = new QBoard(
   document.querySelector("#QBoard"),
   document.querySelector("#BaseQBoard"),
   1600,
@@ -14,6 +18,6 @@ const qboard = new QBoard(
 );
 
 ReactDOM.render(
-  <Overlay qboard={qboard} />,
+  <Overlay qboard={window.qboard} />,
   document.querySelector("#Overlay")
 );
