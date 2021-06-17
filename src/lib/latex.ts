3d7a9dc21034598fcec05e35532c5671946bcc9a
pihart <avi.mehra1@gmail.com>
2021-06-16 20:10:51 -0700

    Retry LaTeX in math mode on certain errors
    Resolves https://github.com/cjquines/qboard/issues/185

--- src/lib/latex.ts
+++ src/lib/latex.ts
@@ -50,16 +50,28 @@ function getErrorFromSVG(
 }
 
 /**
- * Tries to render the TeX from {@param tex} as an SVG in text mode
+ * Tries to render the TeX from {@param tex} as an SVG in text mode.
+ * If MathJax refused to properly render the TeX due to an error ending in "allowed only in math mode",
+ * retry in math mode.
  * @return The MathJax response as a data URL in the form of `image/svg+xml`
- * @throws [[`LaTeXError`]] if MathJax refused to properly render the TeX due to an error
+ * @throws [[`LaTeXError`]] from the first attempt if both attempts have an error (or if there is no second attempt)
  */
 export default function TeXToDataURL(
   tex: string
 ): `data:image/svg+xml,${string}` {
-  const SVG = TeXToSVG(`\\text{${tex}}`);
+  let SVG = TeXToSVG(`\\text{${tex}}`);
 
-  const error = getErrorFromSVG(SVG);
+  let error = getErrorFromSVG(SVG);
+
+  if (error?.errorText.endsWith("allowed only in math mode")) {
+    // Retry without enclosing in `\text{}`
+    // This assignment is okay because we only read this value in cases of success
+    SVG = TeXToSVG(tex);
+
+    // Don't replace `error` if the retry fails;
+    // the first attempt's error has more meaning
+    if (getErrorFromSVG(SVG) === null) error = null;
+  }
 
   if (error !== null) {
     throw new LaTeXError(
