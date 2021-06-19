d22a53c8595c1a27ee72c5c47b8f3da41bd1c09b
pihart <avi.mehra1@gmail.com>
2021-06-19 14:23:20 -0700

    Delete unnecessary comment
    https://github.com/cjquines/qboard/blob/fe6fdc3b9f5345b451262930e22652fb6f57a5b3/src/index.js#L10-L12

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import QBoard from "./lib/qboard";
 
 import "./main.scss";
 
-// The live [[`QBoard`]] instance, exposed to the public as a basic developer interface.
-// Technically, the developer-user gets less control over private fields,
-// but from a power user standpoint this is likely not an issue;
-// no user should be writing scripts that depend on private fields.
+// The live [[`QBoard`]] instance, exposed to the public as a basic developer interface
 window.qboard = new QBoard(
   document.querySelector("#QBoard"),
   document.querySelector("#BaseQBoard"),
